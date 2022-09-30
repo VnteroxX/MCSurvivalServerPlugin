e8e4d303ec3d52fbb5b562fd4591ca1944532985
vnter <kolja@vonrintelen.de>
2022-10-01 01:40:53 +0200

    irgendwie funktioniert die namensänderung vom backpack item noch nicht...

--- src/main/java/me/luciid/survivalserver/backpack/BackpackCommand.java
+++ src/main/java/me/luciid/survivalserver/backpack/BackpackCommand.java
@@ -1,11 +1,14 @@
 package me.luciid.survivalserver.backpack;
 
+import me.luciid.survivalserver.utils.ItemBuilder;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -26,15 +29,19 @@ public class BackpackCommand implements CommandExecutor {
             if(player.getInventory().getItemInMainHand().getType() == Material.BOOK) {
                 if(player.getInventory().getItemInMainHand().getAmount() == 1) {
 
-                    ItemStack backpackItem = player.getItemInHand();
+
+                    ItemStack backpackItem = player.getInventory().getItemInMainHand();
                     String backpackName = args[0];
                     ItemMeta backpackMeta = backpackItem.getItemMeta();
 
                     ArrayList<String> backpackLore = new ArrayList<String>();
-                    backpackLore.add("§eRucksack von §6" + player.getDisplayName());
 
+                    backpackLore.add("§eRucksack von §6" + player.getDisplayName());
+                    backpackLore.set(1, "Debug");
                     backpackMeta.setDisplayName(backpackName);
                     backpackMeta.setLore(backpackLore);
+
+                    //player.getInventory().setItem(player.getInventory().getHeldItemSlot(), backpackItem);
 
                     player.sendMessage("§aDer Rucksack §6" + backpackName + " §awurde erfolgreich erstellt!");
 
